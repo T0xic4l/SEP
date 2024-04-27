003f5d760aa67fa02667a8c8d0ef7209db4fb998
Elias Kaiser <elias@MBP-von-Elias.fritz.box>
2024-04-27 21:27:57 +0200

    Ein paar Sachen gemacht, hehehe

--- Code/Framework/includes/main.h
+++ Code/Framework/includes/main.h
@@ -4,14 +4,33 @@
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 #include <argparse/argparse.hpp>
+#include <string>
 
 /*
- * @brief:  Erstellt anhängig von der Anzahl der Lieferfahrer eine JSON-Objekt mit den driver_actions,
- *          wobei die dazugehörigen Listen leer sind.
- * @param:  Anzahl der Lieferfahrer
- * @return: JSON-Objekt, das die driver_actions enthält
+ * @brief: Startet die Simulation auf dem Server, indem ihm der seed mitgegeben wird
+ * @param host – Der host des Servers
+ * @param seed – Der Seed, der die Simulation generiert
+ * @return: Die Antwort des Servers mit der Ausgangssituation der Simulation
  */
-nlohmann::json initialize_driver_actions(unsigned long driver_count);
+nlohmann::json start_simulation(const std::string & host, const std::string & seed);
+
+/*
+ * @brief: Das erste Entscheidungsverfahren, nach dem die Lieferungen den Lieferfahrern zugewiesen werden. Es wird
+ * nach folgendem Prinzip entschieden:
+ *     Der erste Lieferfahrer, der noch genug Restkapazität für die neuste Bestellung hat, nimmt diese
+ *     auch auf!
+ * @param driver_actions – Eine Map, wo die Aufgaben für die jeweiligen Lieferfahrer eingetragen werden
+ * @param response – Die Antwort des Servers. Dort befindet sich auch das aktuellste Event.
+ * @param driver_count – Anzahl der Lieferfahrer in der Simulation
+ * @return: Die Map mit den neu eingetragenen Zuweisungen für die Lieferfahrer
+ */
+std::map<std::string, std::vector<std::string>> pack_the_bags(std::map<std::string, std::vector<std::string>> & driver_actions, nlohmann::json & response, int driver_count);
+
+bool has_sufficient_capacity(int current_load, int capacity_of_order, int driver_capacity);
+
+std::string construct_proper_id(std::string id, const std::string & id_type);
+
+std::map<std::string, std::vector<std::string>> delete_corresponding_order(std::map<std::string, std::vector<std::string>> & driver_actions, const std::string & id_of_driver);
 
 
 #endif //JUMMYSALESMAN_MAIN_H
--- Code/Framework/src/main.cpp
+++ Code/Framework/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <string>
 #include "../includes/main.h"
 
 
@@ -8,38 +9,90 @@
 int main(int argc, char * argv[]) {
     argparse::ArgumentParser arpa("JummySalesman", "0");
     arpa.add_argument("--seed")
-        .help("Generiert ein einzigartiges Szenario für die Simulation")
-        .default_value("AAA123");
+            .help("Generiert ein einzigartiges Szenario für die Simulation")
+            .default_value("AAA123");
     arpa.add_argument("--host")
-        .help("Url des Server, zu dem eine Verbindung aufgebaut wird")
-        .default_value("http://127.0.0.1:1234");
+            .help("Url des Server, zu dem eine Verbindung aufgebaut wird")
+            .default_value("http://127.0.0.1:1234");
     arpa.parse_args(argc, argv);
 
     auto arg_seed = arpa.get<>("--seed");
     auto arg_host = arpa.get<>("--host");
 
-    nlohmann::json seed = nlohmann::json::parse(R"({"seed":")" + arg_seed + "\"}"); // Seed der an den server gegeben wird
+    nlohmann::json first_response = start_simulation(arg_host, arg_seed);
+    int driver_count = int(first_response["drivers"].size());
 
-    cpr::Response response = cpr::Post(cpr::Url{arg_host + "/"}, // Adresse des servers
-                                cpr::Body{seed.dump()}, // Übergabe des Seeds
-                                cpr::Header{{"Content-Type","application/json"}}); //Definiert, dass gesendete daten im JSON format sind
+    std::map<std::string, std::vector<std::string>> driver_actions;
+    std::map<std::string, int> expected_capacities; // Tracke die momentane Rücksack-Füllmenge eines jeden Lieferfahrers !!!!!!!!!!!!!!!!
 
-    nlohmann::json parsed_response = nlohmann::json::parse(response.text); // Übersetzen des klartexts in ein JSON Objekt
+    while(true) {
+        nlohmann::json actions_to_send;
+        actions_to_send["driver_actions"] = driver_actions;
 
-    unsigned long driver_count = parsed_response["drivers"].size();
+        cpr::Response response = cpr::Post(cpr::Url{arg_host + "/step"},
+                                           cpr::Body{actions_to_send.dump()},
+                                           cpr::Header{{"Content-Type","application/json"}});
 
-    response = cpr::Post(cpr::Url{arg_host + "/step"},  // Initialer Step-Post mit leerer Liste an driver_actions
-                  cpr::Body{initialize_driver_actions(driver_count).dump()},
-                  cpr::Header{{"Content-Type","application/json"}});
-    std::cout << nlohmann::json::parse(response.text) << std::endl;
+        nlohmann::json parsed_response = nlohmann::json::parse(response.text);
+        std::cout << parsed_response << std::endl;
+        std::cout << parsed_response["event"] << std::endl;
+        std::cout << parsed_response["mandatory_actions"] << std::endl;
+
+        if(parsed_response["event"] == "{}") {     // Sendet der Server kein Event, terminiert das Programm – Struktur eines leeren Events unklar!
+            break;
+        }
+
+        driver_actions = pack_the_bags(driver_actions, parsed_response, driver_count);
+    }
 
     return 0;
 }
 
-nlohmann::json initialize_driver_actions(const unsigned long driver_count) {
-    nlohmann::json actions;
-    for(int i = 0; i < driver_count; i++){
-        actions["driver_actions"][std::to_string(i)] = std::vector<std::string>();
+nlohmann::json start_simulation(const std::string & host, const std::string & seed) {
+    nlohmann::json seed_json = nlohmann::json::parse(R"({"seed":")" + seed + "\"}");
+
+    cpr::Response response = cpr::Post(cpr::Url{host + "/"},
+                                       cpr::Body{seed_json.dump()},
+                                       cpr::Header{{"Content-Type","application/json"}});
+    return nlohmann::json::parse(response.text);
+}
+
+std::map<std::string, std::vector<std::string>> pack_the_bags(std::map<std::string, std::vector<std::string>> & driver_actions, nlohmann::json & response, int driver_count) {
+    nlohmann::json event = response["event"];
+    nlohmann::json event_type = event["event_type"];
+    nlohmann::json drivers = response["drivers"];
+
+    if(event_type == "order") {
+        std::string reg_id = construct_proper_id(event["data"]["id"].dump(), "pickup_");
+        for(auto driver : drivers) {
+            if(has_sufficient_capacity(int(driver["current_load"]), int(event["data"]["capacity"]), int(driver["capacity"]))) {
+                driver_actions[driver["id"].dump()].insert(driver_actions[driver["id"].dump()].end(), reg_id);
+                break;
+            } else {
+                continue;
+            }
+        }
+    } else if(event_type == "pickup") {
+        std::string reg_id = construct_proper_id(event["data"]["order_id"].dump(), "dropoff_");
+        std::string id_of_driver = event["data"]["driver"].dump();
+
+        driver_actions[id_of_driver].insert(driver_actions[id_of_driver].end(), reg_id);
+        driver_actions = delete_corresponding_order(driver_actions, id_of_driver);
     }
-    return actions;
+
+    return driver_actions;
+}
+
+bool has_sufficient_capacity(const int current_load, const int capacity_of_order, const int driver_capacity) {
+    return (current_load + capacity_of_order <= driver_capacity);
+}
+
+std::string construct_proper_id(std::string id, const std::string & id_type) {
+    id.erase(std::remove(id.begin(), id.end(), '\"'), id.end());
+    return id_type + id;
+}
+
+std::map<std::string, std::vector<std::string>> delete_corresponding_order(std::map<std::string, std::vector<std::string>> & driver_actions, const std::string & id_of_driver) {
+    driver_actions[id_of_driver].erase(driver_actions[id_of_driver].begin());
+    return driver_actions;
 }
